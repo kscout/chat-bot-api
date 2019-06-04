3d94baeb84625bbe4774fe41ad57a81a4a203242
Vaishnavi Hire <hire.v@husky.neu.edu>
2019-06-04 13:38:02 -0400

    functions for intent

--- scoutBot/features/intent.py
+++ scoutBot/features/intent.py
@@ -11,3 +11,17 @@ class Intent:
     def list_intents(self):
         response = config.service.list_intents(workspace_id=os.environ['WORKSPACE_ID'], export=True)
         return json.dumps(response)
+
+    def update_intent(self, updateIntentData):
+        response = config.service.update_intent(updateIntentData)
+        return json.dumps(response)
+
+    def get_intent(self,intent):
+        response =  config.service.get_intent(workspace_id=os.environ['WORKSPACE_ID'], intent=intent,export=True)
+        return json.dumps(response)
+
+    def delete_intent(self, intent):
+        response= config.service.delete_intent(workspace_id=os.environ['WORKSPACE_ID'], intent=intent)
+
+
+
