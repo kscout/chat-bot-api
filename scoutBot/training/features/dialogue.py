7bc21e2cd108080248166a16bf1383423ff1fdd2
Vaishnavi Hire <hire.v@husky.neu.edu>
2019-06-06 17:13:38 -0400

    removed extra comments

--- scoutBot/training/features/dialogue.py
+++ scoutBot/training/features/dialogue.py
@@ -2,48 +2,34 @@ from scoutBot import config
 import json
 import os
 
+
 class Dialogue:
 
-    def create_dialogue(self,dialogueNodeData):
-        # """
-        #
+    def create_dialogue(self, dialogueNodeData):
         # :param dialogueNodeData: json file from DialogueNodes folder containing all parameters to define dialogue
         # :return: json dump of created node
-        # """
         response = config.service.create_dialog_node(dialogueNodeData)
         return json.dumps(response)
 
     def list_dialogue(self):
-        # """
         # :return: list of all dialogue nodes in the given workspace
-        # """
         response = config.service.list_dialog_nodes(workspace_id=os.environ['WORKSPACE_ID'])
         return json.dumps(response)
 
-    def get_dialogue(self,dialogue_id):
-        # """
-        #
+    def get_dialogue(self, dialogue_id):
         # :param dialogue_id: Unique id of the dialogue
         # :return: json dump of retrived node
-        # """
-        response= config.service.get_dialog_node(workspace_id=os.environ['WORKSPACE_ID'], dialog_node=dialogue_id)
+        response = config.service.get_dialog_node(workspace_id=os.environ['WORKSPACE_ID'], dialog_node=dialogue_id)
         return json.dumps(response)
 
-    def update_dialogue(self,dialogueNodeData):
-        # """
-        #
+    def update_dialogue(self, dialogueNodeData):
         # :param dialogueNodeData: json file from DialogueNodes folder containing all parameters to define dialogue
         # :return: json dump of updated node
-        # """
         response = config.service.update_dialog_node(dialogueNodeData)
         return json.dumps(response)
 
-    def delete_dialogue(self,dialogue_id):
-        # """
-        #
+    def delete_dialogue(self, dialogue_id):
         # :param dialogue_id: unique id of the dialogue node
         # :return: json dump of the deleted node
-        # """
-        response= config.service.delete_dialog_node(workspace_id=os.environ['WORKSPACE_ID'], dialog_node=dialogue_id)
+        response = config.service.delete_dialog_node(workspace_id=os.environ['WORKSPACE_ID'], dialog_node=dialogue_id)
         return json.dumps(response)
-
--- scoutBot/training/features/intent.py
+++ scoutBot/training/features/intent.py
@@ -5,29 +5,33 @@ import os
 class Intent:
 
     def create_intent(self, intentData):
-        """
-
-        :param intentData:
-        :return:
-        """
+        # :param intentData: json file from intentData folder containing all parameters to define dialogue
+        # :return: json dump of created node
         response = config.service.create_intent(intentData)
         return json.dumps(response)
 
     def list_intents(self):
+        # :return: json dump of all nodes
         response = config.service.list_intents(workspace_id=os.environ['WORKSPACE_ID'], export=True)
         return json.dumps(response)
 
     def update_intent(self, updateIntentData):
+        # :param intentData: json file from intentData folder containing all parameters to define dialogue
+        # :return: json dump of updated node
         response = config.service.update_intent(updateIntentData)
         return json.dumps(response)
 
     def get_intent(self,intent):
+        # :param intent: unique intent name
+        # :return: json dump of given node
         response =  config.service.get_intent(workspace_id=os.environ['WORKSPACE_ID'], intent=intent, export=True)
         return json.dumps(response)
 
     def delete_intent(self, intent):
+        # :param intent: unique intent name
+        # :return: json dump of given node
         response= config.service.delete_intent(workspace_id=os.environ['WORKSPACE_ID'], intent=intent)
-        return
+        return json.dumps(response)
 
 
 
