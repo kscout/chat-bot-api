64f2ae8bb982cf6d8a6622c9b1bad6b8c9bece89
Vaishnavi Hire <hire.v@husky.neu.edu>
2019-06-10 16:21:54 -0400

    user context added

--- processmessage.py
+++ processmessage.py
@@ -2,11 +2,13 @@ from controllers import search
 from config import config
 import os
 import json
+from flask import session
 
 
 def identify_actions(response: json, message: str) -> str:
     if 'actions' in response['output'] and response['output']['actions'][0]['type'] == 'client':
         if 'search' == response['output']['actions'][0]['name']:
+            # message = response['context']['context_1']
             return search.search_apps(message)
 
 
@@ -24,10 +26,23 @@ def process_message(message: str) -> str:
         'message_type:': 'text',
         'text': message
     }
+    print(message_input)
 
-    # Response from watson api
-    response = config.service.message(
-        workspace_id=os.environ['WORKSPACE_ID'],
-        input=message_input).get_result()
+    print(config.USER_CONTEXT)
+    if(not config.USER_CONTEXT):
+        response = config.service.message(
+            workspace_id=os.environ['WORKSPACE_ID'],
+            input=message_input).get_result()
+    else:
+        # Response from watson api
+        print("*************")
+        print(config.USER_CONTEXT)
+        response = config.service.message(
+            workspace_id=os.environ['WORKSPACE_ID'],
+            input=message_input, context=config.USER_CONTEXT).get_result()
+
+    config.USER_CONTEXT = response['context']
+
+    print(response)
 
     return identify_actions(response, message) or identify_generic_output(response)
