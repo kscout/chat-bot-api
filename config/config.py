134fd37cbfc1d48666a8a4cc754c45b63aa765ff
Vaishnavi Hire <hire.v@husky.neu.edu>
2019-07-01 17:01:30 -0400

    update new app_id, categories and tags

--- config/config.py
+++ config/config.py
@@ -12,24 +12,6 @@ service = ibm_watson.AssistantV1(
 USER_CONTEXT = ""
 
 
-## Logger Configuration ##
-logger = logging.getLogger('Logger')
-logger.setLevel(logging.DEBUG)
-
-# create console handler and set level to debug
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-
-# create formatter
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-# add formatter to ch
-ch.setFormatter(formatter)
-
-# add ch to logger
-logger.addHandler(ch)
-
-
 # Configuring Database ENV
 db_config = dict()
 db_config["DB_HOST"] = os.environ['APP_DB_HOST']
